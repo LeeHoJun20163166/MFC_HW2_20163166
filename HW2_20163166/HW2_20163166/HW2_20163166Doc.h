11624fed81cd5f25880888cd1113155009b5a87d
호준 이 <chunskm@bu.ac.kr>
2020-10-30 21:44:17 +0900

    과제

---
+++ HW2_20163166/HW2_20163166/HW2_20163166Doc.h
@@ -0,0 +1,48 @@
+﻿
+// HW2_20163166Doc.h: CHW220163166Doc 클래스의 인터페이스
+//
+
+
+#pragma once
+
+
+class CHW220163166Doc : public CDocument
+{
+protected: // serialization에서만 만들어집니다.
+	CHW220163166Doc() noexcept;
+	DECLARE_DYNCREATE(CHW220163166Doc)
+
+// 특성입니다.
+public:
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+// 구현입니다.
+public:
+	virtual ~CHW220163166Doc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+	// 검색 처리기에 대한 검색 콘텐츠를 설정하는 도우미 함수
+	void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+};
---
+++ HW2_20163166/HW2_20163166/HW2_20163166View.cpp
@@ -0,0 +1,105 @@
+﻿
+// HW2_20163166View.cpp: CHW220163166View 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "HW2_20163166.h"
+#endif
+
+#include "HW2_20163166Doc.h"
+#include "HW2_20163166View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CHW220163166View
+
+IMPLEMENT_DYNCREATE(CHW220163166View, CView)
+
+BEGIN_MESSAGE_MAP(CHW220163166View, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CHW220163166View 생성/소멸
+
+CHW220163166View::CHW220163166View() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CHW220163166View::~CHW220163166View()
+{
+}
+
+BOOL CHW220163166View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CHW220163166View 그리기
+
+void CHW220163166View::OnDraw(CDC* /*pDC*/)
+{
+	CHW220163166Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+
+// CHW220163166View 인쇄
+
+BOOL CHW220163166View::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CHW220163166View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CHW220163166View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+
+// CHW220163166View 진단
+
+#ifdef _DEBUG
+void CHW220163166View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CHW220163166View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CHW220163166Doc* CHW220163166View::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CHW220163166Doc)));
+	return (CHW220163166Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CHW220163166View 메시지 처리기
